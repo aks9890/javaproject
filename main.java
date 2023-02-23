86a33afdbfb4fbc2e1faf883013951257eddee89
aks9890 <akshaynagtilak123@gmail.com>
2023-02-23 15:16:49 +0530

    add pls

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 // Your First Program
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World,Good morning have a nice dayyyyyyyyyyyyyyyyyyy!"); 
+        System.out.println("Hellooooooo, World,Good morning have a nice dayyyyyyyyyyyyyyyyyyy!"); 
     }
 }
