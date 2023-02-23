b79206e5b50cd9239198be8cb621e02ea5270006
aks9890 <akshaynagtilak123@gmail.com>
2023-02-23 15:14:34 +0530

    webhook added

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 // Your First Program
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World,Good morning have a nice day!"); 
+        System.out.println("Hello, World,Good morning have a nice dayyyyyyyyyyyyyyyyyyy!"); 
     }
 }
